501740f4a7b15b23ed510f4298d52b3d01a682eb
lenovoy <337906069@qq.com>
2014-09-26 04:37:18 +0800

    completed

--- WEB-INF/src/com/servlet/login/ApesServlet.java
+++ WEB-INF/src/com/servlet/login/ApesServlet.java
@@ -14,6 +14,15 @@ public class ApesServlet extends HttpServlet {
 	/**
 	 * 
 	 */
+	
+	private final static int DIRECTOR=0;
+	private final static int WRITER=1;
+	private final static int ACTOR=2;
+	private final static int TYPE=3;
+	private final static int LOCATION=4;
+	private final static int LANGUAGE=5;
+	private final static int DATE=6;
+	private final static int MIN=7;
 	private static final long serialVersionUID = 1L;
 
 	public ApesServlet(){
@@ -37,29 +46,29 @@ public class ApesServlet extends HttpServlet {
 		String itemName=req.getParameter("itemName");
 		
 		OutputStreamWriter out=new OutputStreamWriter(resp.getOutputStream());
-		switch(itemName){
-		case "Director":
+		switch(Integer.parseInt(itemName)){
+		case DIRECTOR:
 			out.write(URLEncoder.encode("马特·里夫斯","utf-8"));
 			break;
-		case "Writer":
+		case WRITER:
 			out.write(URLEncoder.encode("马克·鲍姆贝克 / 里克·杰法 / 阿曼达·斯尔沃","utf-8"));
 			break;
-		case "Actor":
+		case ACTOR:
 			out.write(URLEncoder.encode("安迪·瑟金斯 / 杰森·克拉科 / 加里·奥德曼 / 凯丽·拉塞尔 / 托比·凯贝尔 ","utf-8"));
 			break;
-		case "Type":
+		case TYPE:
 			out.write(URLEncoder.encode("剧情 / 动作 / 科幻","utf-8"));
 			break;
-		case "Location":
+		case LOCATION:
 			out.write(URLEncoder.encode("美国","utf-8"));
 			break;
-		case "Language":
+		case LANGUAGE:
 			out.write(URLEncoder.encode("英语 / 美国手语","utf-8"));
 			break;
-		case "Date":
+		case DATE:
 			out.write(URLEncoder.encode("2014-08-29(中国大陆) / 2014-07-11(美国)","utf-8"));
 			break;
-		case "Min":
+		case MIN:
 			out.write(URLEncoder.encode("130 分钟","utf-8"));
 			break;
 		}
